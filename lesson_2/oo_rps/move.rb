2799fca0ba74e23537ebff4704347a70c873e38b
Hugo Ma <huwgoma@gmail.com>
2024-01-29 22:31:58 -0500

    Write outline for Move refactoring

--- lesson_2/oo_rps/move.rb
+++ lesson_2/oo_rps/move.rb
@@ -1,5 +1,29 @@
 require_relative 'class_converter'
 
+# Get rid of class converter
+# Create a new class: MoveFactory
+# - Takes an input string and creates/returns the corresponding move subclass object
+# eg. MoveFactory.create_move(type, owner)
+# Given a string (type) and a player object (owner):
+#   Verify that type is a key in MOVE_TYPES - if not, raise error (? necessary? we are already verifying input )
+#   If type is indeed a key:
+#     Create and return the object of the corresponding type (eg. 'Rock' => new Rock object )
+
+# Data:
+# Hash containing the String-Constant conversions, MOVE_TYPES
+#   { 'Rock' => Rock, 'Paper' => Paper, ... }
+# 
+
+# Comparing Moves
+# Each class should maintain WINS_AGAINST hash
+#   { 'Scissors' => 'crushes' }
+# Move#<=>
+# - Compare using @type (self.class.to_s) - string representation of the move's type
+#   => 0 if @type == other_move.type
+#   => 1 if self.WINS_AGAINST contains a key matching the other_move.type 
+
+
+
 # Move Superclass
 class Move
   include ClassConverter
