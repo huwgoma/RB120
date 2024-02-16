d5cea5247661cff705498a255e4821c5251c3adb
Hugo Ma <huwgoma@gmail.com>
2024-02-15 22:33:07 -0500

    OO TTT - Initial Commit after walkthrough

--- lesson_5/oo_ttt/ttt_design.rb
+++ lesson_5/oo_ttt/ttt_design.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # 1) Describe the problem 
 # 2) Extract nouns and verbs from problem description
 # 3) Organize the verbs with the performing nouns
@@ -23,17 +24,23 @@
 # 4) Spike
 class Board
   GRID_LENGTH = 3
+  WIN_CONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
+              [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
+              [1, 5, 9], [3, 5, 7]]             # diagonals
+
+  attr_reader :squares
 
   def initialize
     @squares = create_squares
   end
 
   def display
-    puts " #{value_at_square(1)} | #{value_at_square(2)} | #{value_at_square(3)} "
+    system 'clear'
+    puts " #{value_at(1)} | #{value_at(2)} | #{value_at(3)} "
     puts "---+---+---"
-    puts " #{value_at_square(4)} | #{value_at_square(5)} | #{value_at_square(6)} "
+    puts " #{value_at(4)} | #{value_at(5)} | #{value_at(6)} "
     puts "---+---+---"
-    puts " #{value_at_square(7)} | #{value_at_square(8)} | #{value_at_square(9)} "
+    puts " #{value_at(7)} | #{value_at(8)} | #{value_at(9)} "
   end
   # 3x3 grid of 'squares'
   # - each square can be represented with a Square object
@@ -45,10 +52,41 @@ class Board
     end
   end
 
-  def value_at_square(key)
-    @squares[key].value
+  def value_at(key)
+    squares[key].value
   end
 
+  def mark_at(key, value)
+    squares[key].mark(value)
+  end
+
+  def unmarked_keys
+    squares.keys.select { |key| squares[key].empty? }
+  end
+
+  def full?
+    unmarked_keys.empty?
+  end
+
+  def has_winner?(current_marker)
+    !!detect_winner(current_marker)
+  end
+
+  # Return the marker of the winner, or nil if no winner
+  def detect_winner(current_marker)
+    WIN_CONS.each do |line|
+      return current_marker if winning_line?(line, current_marker)
+    end
+    nil
+  end
+
+  def winning_line?(line, marker)
+    squares.values_at(*line).all? { |square| square.value == marker }
+  end
+
+  def reset
+    squares.values.each(&:unmark)
+  end
 end
 
 
@@ -60,12 +98,34 @@ class Square
   def initialize
     @value = INITIAL_VALUE
   end
+
+  def mark(value)
+    self.value = value
+  end
+
+  def unmark
+    self.value = INITIAL_VALUE
+  end
+
+  def empty?
+    value == INITIAL_VALUE
+  end
+
+  private
+
+  attr_writer :value
   # Track the state of the square 
   # - Default state is empty (nil?)
   # - Can be marked by either 'X' or 'O' (strings?)
 end
 
 class Player
+  attr_reader :marker, :board
+
+  def initialize(marker, board)
+    @marker = marker
+    @board = board
+  end
   # States: 
   # - Player's marker (X or O)
   # Behaviors:
@@ -74,32 +134,95 @@ class Player
   #     square (by updating that square's state)
 end
 
+class Human < Player
+  def choose_move
+    puts "Choose an empty square (#{board.unmarked_keys.join(', ')}):"
+    loop do
+      num = gets.chomp.to_i
+      return num if board.unmarked_keys.include?(num)
+      puts "Sorry, that's not a valid choice."
+    end
+  end
+end
+
+class Computer < Player
+  def choose_move
+    board.unmarked_keys.sample
+  end
+end
+
 class TTTGame
-  attr_reader :board
+  attr_reader :board, :human, :computer
 
   def initialize
     @board = Board.new
+    @human = Human.new('X', board)
+    @computer = Computer.new('O', board)
   end
 
   def play
     display_welcome
-    # set current player (random?)
-    
+    gets.chomp #pause
+
+    board.display
+    set_current_player
+    # Program Loop
     loop do
       board.display
-      # display_board #(game state)
-      # current_player.choose_move
-      # break if someone_won? || board_full?
-      # switch current player
-      break
+      
+      # Game Loop
+      loop do
+        # display game state
+        board.mark_at(current_player.choose_move, current_player.marker)
+        board.display
+        break if board.full? || board.has_winner?(current_player.marker)
+
+        switch_current_player
+      end
+
+      display_result
+      break unless play_again?
+      board.reset
     end
 
-    # display game result
     display_goodbye
+
   end
 
+
   def display_welcome
+    system('clear')
     puts "Welcome to Tic Tac Toe!"
+  end
+
+  def set_current_player
+    @current_player, @next_player = human, computer
+  end
+
+  def switch_current_player
+    self.current_player, self.next_player = next_player, current_player
+  end
+
+  private
+
+  attr_accessor :current_player, :next_player
+
+  def display_result
+    case board.detect_winner(current_player.marker)
+    when human.marker    then puts 'You won!'
+    when computer.marker then puts 'Computer won!'
+    else                      puts "It's a tie!"
+    end
+
+  end
+
+  def play_again?
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      return answer == 'y' if %w(y n).include?(answer)
+      puts "Sorry - please enter y or n."
+    end
   end
 
   def display_goodbye
