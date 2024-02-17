d252b8022b7b7f5ed0837166820edaaf5767ad45
Hugo Ma <huwgoma@gmail.com>
2024-02-17 13:49:13 -0500

    Update Board#draw to be scalable to different board sizes (not WINCONS yet); remove Board#value_at method.

--- lesson_5/oo_ttt/board.rb
+++ lesson_5/oo_ttt/board.rb
@@ -4,7 +4,10 @@ require_relative 'displayable'
 class Board
   include Displayable
   
-  GRID_LENGTH = 3
+  GRID_LENGTH = 4
+  GRID_SIZE = GRID_LENGTH ** 2
+
+  # Update Win cons to be scalable too
   WIN_CONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
               [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
               [1, 5, 9], [3, 5, 7]]             # diagonals
@@ -15,12 +18,28 @@ class Board
     @squares = create_squares
   end
 
+  # Board Drawing Methods
   def draw
-    puts " #{value_at(1)} | #{value_at(2)} | #{value_at(3)} "
-    puts "---+---+---"
-    puts " #{value_at(4)} | #{value_at(5)} | #{value_at(6)} "
-    puts "---+---+---"
-    puts " #{value_at(7)} | #{value_at(8)} | #{value_at(9)} "
+    cell_size = calculate_cell_size
+    row_partition = create_row_partition
+    column_partition = '|'
+
+    squares.each do |key, square|
+      partition = (key % GRID_LENGTH).zero? ? row_partition : column_partition
+      cell_value = square.empty? ? key.to_s : square.value
+      print cell_value.center(cell_size, ' ')
+      print partition unless key >= GRID_SIZE
+    end
+
+    puts "\n"
+  end
+
+  def calculate_cell_size
+    [3, squares.keys.max.to_s.size].max
+  end
+
+  def create_row_partition
+    "\n#{('---+' * GRID_LENGTH).chop}\n"
   end
 
   def clear_and_draw
@@ -29,13 +48,9 @@ class Board
   end
 
   def create_squares
-    (1..GRID_LENGTH ** 2).each_with_object({}) do |key, squares|
+    (1..GRID_SIZE).each_with_object({}) do |key, squares|
       squares[key] = Square.new
     end
-  end
-
-  def value_at(key)
-    squares[key].value
   end
 
   def mark_at(key, value)
