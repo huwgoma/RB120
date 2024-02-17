caceaa234d2f75c73a8208e4835672593d52408c
Hugo Ma <huwgoma@gmail.com>
2024-02-17 16:33:57 -0500

    Refactor win condition calculation to be scalable to the board size

--- lesson_5/oo_ttt/board.rb
+++ lesson_5/oo_ttt/board.rb
@@ -4,18 +4,44 @@ require_relative 'displayable'
 class Board
   include Displayable
   
-  GRID_LENGTH = 4
-  GRID_SIZE = GRID_LENGTH ** 2
+  GRID_LENGTH = 3
+  GRID_AREA = GRID_LENGTH ** 2
 
-  # Update Win cons to be scalable too
-  WIN_CONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
-              [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
-              [1, 5, 9], [3, 5, 7]]             # diagonals
-
-  attr_reader :squares
+  attr_reader :squares, :win_conditions
 
   def initialize
     @squares = create_squares
+    @win_conditions = calculate_win_conditions
+  end
+
+  # Calculating win conditions of the board
+  def calculate_win_conditions
+    winning_rows = calculate_winning_rows
+    winning_cols = calculate_winning_cols
+    winning_diagonals = calculate_winning_diagonals
+
+    winning_rows + winning_cols + winning_diagonals
+  end
+
+  def calculate_winning_rows
+    (1..GRID_AREA).each_slice(GRID_LENGTH).to_a
+  end
+
+  def calculate_winning_cols
+    (1..GRID_LENGTH).map do |origin|
+      column = [origin]
+      column << origin += GRID_LENGTH until column.size == GRID_LENGTH
+      column
+    end
+  end
+
+  def calculate_winning_diagonals
+    [1, GRID_LENGTH].map do |origin|
+      diagonal = [origin]
+      increment = origin == 1 ? (GRID_LENGTH + 1) : (GRID_LENGTH - 1)
+      diagonal << origin += increment until diagonal.size == GRID_LENGTH
+      diagonal
+    end
   end
 
   # Board Drawing Methods
@@ -28,9 +54,8 @@ class Board
       partition = (key % GRID_LENGTH).zero? ? row_partition : column_partition
       cell_value = square.empty? ? key.to_s : square.value
       print cell_value.center(cell_size, ' ')
-      print partition unless key >= GRID_SIZE
+      print partition unless key >= GRID_AREA
     end
-
     puts "\n"
   end
 
@@ -47,8 +72,11 @@ class Board
     draw
   end
 
+
+
+
   def create_squares
-    (1..GRID_SIZE).each_with_object({}) do |key, squares|
+    (1..GRID_AREA).each_with_object({}) do |key, squares|
       squares[key] = Square.new
     end
   end
@@ -71,7 +99,7 @@ class Board
 
   # Return the marker of the winner, or nil if no winner
   def winning_marker
-    WIN_CONS.each do |line|
+    win_conditions.each do |line|
       first_square = squares[line.first]
       next if first_square.empty?
 
